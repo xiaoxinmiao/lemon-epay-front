81d348b386716a45b5d8f0b3934f3f1c83e580bd
xiao.xinmiao <xiaoxm_001@163.com>
2018-01-30 10:56:11 +0800

    pay_sign ==> paySign

--- src/models/pay.js
+++ src/models/pay.js
@@ -48,7 +48,7 @@ export default {
                     nonceStr: result.nonceStr, // 支付签名随机串，不长于 32 位
                     package: result.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
                     signType: result.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                    paySign: result.pay_sign, // 支付签名
+                    paySign: result.paySign, // 支付签名
                     success: function (res) {
                         // 支付成功后的回调函数
                     },
